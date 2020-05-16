6d66afdb3d0601f0c39247616c23c35aeae9e652
Mathieu Bretaud <mbretaud@gmail.com>
2020-05-16 22:12:03 +0200

    init project

--- Makefile
+++ Makefile
@@ -1,26 +1,3 @@
 CURRENT_DIR = $(shell pwd)
 
-# Dashboard
-dashboardFile=kubernetes-dashboard-v2.0.0.yaml
-
-# Namespace
-namespaceName=kubernetes-dashboard
-
-$(info ############################################### )
-$(info # )
-$(info # Environment variables )
-$(info # )
-$(info ############################################### )
-$(info CURRENT_DIR: $(CURRENT_DIR))
-
-$(info )
-$(info ############################################### )
-$(info # )
-$(info # Parameters )
-$(info # )
-$(info ############################################### )
-$(info dashboardFile: $(dashboardFile))
-$(info namespaceName: $(namespaceName))
-$(info )
-
 include $(CURRENT_DIR)/make-commons-k8s.mk
--- make-commons-k8s.mk
+++ make-commons-k8s.mk
@@ -1,11 +1,11 @@
 #
 # You need to define
-#   dashboardFile
-#   namespaceName
 
-POD_NAME=$(shell kubectl get pods --output='json' | jq ".items | .[] | .metadata | select(.name | startswith(\"$(deploymentName)\")) | .name" | head -1 | sed 's/"//g')
+all:
 
-all: help
+KEY = $(CURRENT_DIR)/secrets/ssl/tls.key
+CERT = $(CURRENT_DIR)/secrets/ssl/tls.crt
+POD_NAME=$(shell kubectl get pods --namespace=kubernetes-dashboard --output='json' | jq ".items | .[] | .metadata | select(.name | startswith(\"kubernetes-dashboard\")) | .name" | head -1 | sed 's/"//g')
 
 help:
 	@echo ""
@@ -16,6 +16,7 @@ help:
 	@echo "   3. make delete    - delete resources"
 	@echo "   4. make describe  - show details of the resources"
 	@echo "   5. make get       - display one or many resources"
+	@echo "   6. make change	- change namespace"
 	@echo ""
 
 ###############################################
@@ -24,7 +25,14 @@ help:
 #
 ###############################################
 deploy:
-	kubectl create -f $(dashboardFile) --save-config
+	kubectl create -f kubernetes-dashboard-v2.0.0.yaml --save-config
+	@sleep 1
+	mkdir -p $(CURRENT_DIR)/secrets/ssl
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $(KEY) -out $(CERT) -subj "/CN=nginxsvc/O=nginxsvc"
+	@sleep 1
+	kubectl create secret tls nginxsecret --namespace=kubernetes-dashboard --key $(KEY) --cert $(CERT) --save-config
+	@sleep 1
+	kubectl create -f kubernetes-dashboard-ingress.yaml --save-config
 
 ###############################################
 #
@@ -32,7 +40,8 @@ deploy:
 #
 ###############################################
 apply:
-	kubectl apply -f $(dashboardFile)
+	kubectl apply -f kubernetes-dashboard-v2.0.0.yaml
+	kubectl apply -f kubernetes-dashboard-ingress.yaml
 
 ###############################################
 #
@@ -40,7 +49,8 @@ apply:
 #
 ###############################################
 delete:
-	kubectl delete -f $(dashboardFile)
+	kubectl delete -f kubernetes-dashboard-ingress.yaml
+	kubectl delete -f kubernetes-dashboard-v2.0.0.yaml
 
 ###############################################
 #
@@ -49,33 +59,35 @@ delete:
 ###############################################
 describe:
 	@echo "---------------------------"
-	kubectl describe namespace/$(namespaceName)
+	kubectl describe namespace/kubernetes-dashboard
 	@echo ""
-	kubectl describe serviceaccount/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe serviceaccount/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe service/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe service/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe secret/kubernetes-dashboard-certs --namespace=$(namespaceName)
+	kubectl describe secret/kubernetes-dashboard-certs --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe secret/kubernetes-dashboard-csrf --namespace=$(namespaceName)
+	kubectl describe secret/kubernetes-dashboard-csrf --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe secret/kubernetes-dashboard-key-holder --namespace=$(namespaceName)
+	kubectl describe secret/kubernetes-dashboard-key-holder --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe configmap/kubernetes-dashboard-settings --namespace=$(namespaceName)
+	kubectl describe configmap/kubernetes-dashboard-settings --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe role.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe role.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe clusterrole.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe clusterrole.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe rolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe rolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe clusterrolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe clusterrolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe deployment.apps/kubernetes-dashboard --namespace=$(namespaceName)
+	kubectl describe deployment.apps/kubernetes-dashboard --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe service/dashboard-metrics-scraper --namespace=$(namespaceName)
+	kubectl describe service/dashboard-metrics-scraper --namespace=kubernetes-dashboard
 	@echo ""
-	kubectl describe deployment.apps/dashboard-metrics-scraper --namespace=$(namespaceName)
+	kubectl describe deployment.apps/dashboard-metrics-scraper --namespace=kubernetes-dashboard
+	@echo ""
+	kubectl describe ingress.networking.k8s.io/prometheus-ingress --namespace=kubernetes-dashboard
 	@echo "---------------------------"
 
 ###############################################
@@ -85,31 +97,49 @@ describe:
 ###############################################
 get:
 	@echo "---------------------------"
-	kubectl get namespace/$(namespaceName) --ignore-not-found
+	kubectl get namespace/kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get serviceaccount/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get serviceaccount/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get service/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get service/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get secret/kubernetes-dashboard-certs --namespace=$(namespaceName) --ignore-not-found
+	kubectl get secret/kubernetes-dashboard-certs --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get secret/kubernetes-dashboard-csrf --namespace=$(namespaceName) --ignore-not-found
+	kubectl get secret/kubernetes-dashboard-csrf --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get secret/kubernetes-dashboard-key-holder --namespace=$(namespaceName) --ignore-not-found
+	kubectl get secret/kubernetes-dashboard-key-holder --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get configmap/kubernetes-dashboard-settings --namespace=$(namespaceName) --ignore-not-found
+	kubectl get configmap/kubernetes-dashboard-settings --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get role.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get role.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get clusterrole.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get clusterrole.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get rolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get rolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get clusterrolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get clusterrolebinding.rbac.authorization.k8s.io/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get deployment.apps/kubernetes-dashboard --namespace=$(namespaceName) --ignore-not-found
+	kubectl get deployment.apps/kubernetes-dashboard --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get service/dashboard-metrics-scraper --namespace=$(namespaceName) --ignore-not-found
+	kubectl get service/dashboard-metrics-scraper --namespace=kubernetes-dashboard --ignore-not-found
 	@echo ""
-	kubectl get deployment.apps/dashboard-metrics-scraper --namespace=$(namespaceName) --ignore-not-found
+	kubectl get deployment.apps/dashboard-metrics-scraper --namespace=kubernetes-dashboard --ignore-not-found
+	@echo ""
+	kubectl get ingress.networking.k8s.io/prometheus-ingress --namespace=kubernetes-dashboard
 	@echo "---------------------------"
+
+###############################################
+#
+# Change Namespace
+#
+###############################################
+change:
+	kubectl config set-context $(shell kubectl config current-context) --namespace=kubernetes-dashboard
+
+###############################################
+#
+# Get logs from the pod
+#
+###############################################
+logs:
+	kubectl logs pod/$(POD_NAME) --namespace=kubernetes-dashboard
